43fa71fed46b28df24923f38db131dec695fab6b
pramodh <k.nagapramod28@gmail.com>
2023-12-07 16:46:56 +0530

    backend auth completed

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
+const keys = require("./config/keys");
+const authRoutes = require("./routes/authRoutes");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "This is pramodh" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hi: "This is pramodh" });
+// });
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+//Client Id: 945078027571-7q5s9ajjc5h5kfktre02hl6l1lolfmpq.apps.googleusercontent.com
+//Client Secret : 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
+
+mongoose.connect(keys.mongodb);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
