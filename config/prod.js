d5a2df6a2e16f081d0a82c22041ba7ffa700ac01
pramodh <k.nagapramod28@gmail.com>
2023-12-08 20:32:22 +0530

    adding production keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongodb: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
