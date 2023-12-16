9ba98c0aab62474f15da9e5e8c4a723c1f2dc34d
pramodh <k.nagapramod28@gmail.com>
2023-12-16 19:41:44 +0530

    refactored code from promises to async/await and removed consoles

--- services/passport.js
+++ services/passport.js
@@ -25,20 +25,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("entered loop");
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-          })
-            .save()
-            .then((user) => done(null, user));
-          console.log("else case");
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({
+        googleId: profile.id,
+      }).save();
+      done(null, user);
     }
   )
 );
